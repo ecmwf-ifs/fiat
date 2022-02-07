2239cd863c7a4ac2544aaee8518c398ca7a6347b
Olivier Marsden <Olivier.Marsden@ecmwf.int>
2022-02-07 17:44:58 +0000

    Add qsortc character sorting routine used by Meteo France.
    qsortc.F taken from ifsaux/support and added as src/fiat/util/qsortc.F90

---
+++ src/fiat/util/qsortc.F90
@@ -0,0 +1,119 @@
+! (C) Copyright 2005- Meteo France.
+! 
+! This software is licensed under the terms of the Apache Licence Version 2.0
+! which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+! In applying this licence, ECMWF does not waive the privileges and immunities
+! granted to it by virtue of its status as an intergovernmental organisation
+! nor does it submit to any jurisdiction.
+!
+
+! Sort character strings
+SUBROUTINE QSORTC (N,ORD,A)
+USE EC_PARKIND, ONLY : JPIM
+IMPLICIT NONE
+      
+INTEGER(KIND=JPIM), INTENT(IN)    :: N
+INTEGER(KIND=JPIM), INTENT(INOUT) :: ORD(N)
+CHARACTER(LEN=*),   INTENT(IN)    :: A(N)
+
+INTEGER(KIND=JPIM) :: I,IP,IQ,IX,IZ,L,L1,NDEEP,P
+INTEGER(KIND=JPIM) :: POPLST(2,20)
+INTEGER(KIND=JPIM) :: Q,U,U1,YP
+
+CHARACTER(LEN=LEN(A)) :: X,XX,Z,ZZ,Y
+
+      NDEEP=0
+      U1=N
+      L1=1
+      DO 1  I=1,N
+    1 ORD(I)=I
+    2 IF (U1.LE.L1) RETURN
+
+    3 L=L1
+      U=U1
+    4 P=L
+      Q=U
+
+      X=A(ORD(P))
+      Z=A(ORD(Q))
+      IF (X.LE.Z) GO TO 5
+      Y=X
+      X=Z
+      Z=Y
+      YP=ORD(P)
+      ORD(P)=ORD(Q)
+      ORD(Q)=YP
+    5 IF (U-L.LE.1) GO TO 15
+      XX=X
+      IX=P
+      ZZ=Z
+      IZ=Q
+
+    6 P=P+1
+      IF (P.GE.Q) GO TO 7
+      X=A(ORD(P))
+      IF (X.GE.XX) GO TO 8
+      GO TO 6
+    7 P=Q-1
+      GO TO 13
+
+    8 Q=Q-1
+      IF (Q.LE.P) GO TO 9
+      Z=A(ORD(Q))
+      IF (Z.LE.ZZ) GO TO 10
+      GO TO 8
+    9 Q=P
+      P=P-1
+      Z=X
+      X=A(ORD(P))
+
+   10 IF (X.LE.Z) GO TO 11
+      Y=X
+      X=Z
+      Z=Y
+      IP=ORD(P)
+      ORD(P)=ORD(Q)
+      ORD(Q)=IP
+   11 IF (X.LE.XX) GO TO 12
+      XX=X
+      IX=P
+   12 IF (Z.GE.ZZ) GO TO 6
+      ZZ=Z
+      IZ=Q
+      GO TO 6
+
+   13 CONTINUE
+      IF (.NOT.(P.NE.IX.AND.X.NE.XX)) GO TO 14
+      IP=ORD(P)
+      ORD(P)=ORD(IX)
+      ORD(IX)=IP
+   14 CONTINUE
+      IF (.NOT.(Q.NE.IZ.AND.Z.NE.ZZ)) GO TO 15
+      IQ=ORD(Q)
+      ORD(Q)=ORD(IZ)
+      ORD(IZ)=IQ
+   15 CONTINUE
+      IF (U-Q.LE.P-L) GO TO 16
+      L1=L
+      U1=P-1
+      L=Q+1
+      GO TO 17
+   16 U1=U
+      L1=Q+1
+      U=P-1
+   17 CONTINUE
+      IF (U1.LE.L1) GO TO 18
+
+      NDEEP=NDEEP+1
+      POPLST(1,NDEEP)=U
+      POPLST(2,NDEEP)=L
+      GO TO 3
+   18 IF (U.GT.L) GO TO 4
+
+      IF (NDEEP.EQ.0) GO TO 2
+      U=POPLST(1,NDEEP)
+      L=POPLST(2,NDEEP)
+      NDEEP=NDEEP-1
+      GO TO 18
+
+END SUBROUTINE QSORTC
