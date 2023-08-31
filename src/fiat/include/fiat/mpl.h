01e22edb7adbfb971b400a800d2579eb9dd0d2d6
Willem Deconinck <willem.deconinck@ecmwf.int>
2023-08-31 17:44:07 +0200

    Add MPL_COMM_COMPARE

--- src/fiat/include/fiat/mpl.h
+++ src/fiat/include/fiat/mpl.h
@@ -20,6 +20,8 @@ extern "C" {
 int  mpl_init();
 int  mpl_end();
 int  mpl_myrank(); // Note return value is 1-based as opposed to MPI_Rank which is 0-based
+int  mpl_comm();
+int  mpl_comm_oml(int oml_thread); // Note oml_thread argument is 1-based as opposed to omp_threads
 
 #ifdef __cplusplus
 } // extern "C"
