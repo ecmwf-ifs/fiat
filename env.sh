1e9018b99ff8bf3d18755b1bb9e930aa1d0ab15b
Lucian Anton <lucian.anton@ecmwf.int>
2024-11-06 16:40:53 +0200

    files useful to build

---
+++ env.sh
@@ -0,0 +1,9 @@
+ml LUMI/24.03
+ml partition/C
+ml
+export ecbuild_ROOT=../ecbuild
+#export MPI_HOME=<path-to-MPI>
+export fckit_ROOT=../fckit
+#export CC=
+#export FC=<path-to-Fortran-compiler>
+#export CXX=<path-to-C++-compiler> 
