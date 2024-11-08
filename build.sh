b10ca6d83918dad3b600aa22b33bd32625b7764e
Lucian Anton <lucian.anton@ecmwf.int>
2024-11-08 19:17:58 +0200

    basic test for mpl_alltoallv in + some auxilliry files

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 
 bdir=bdir
 
-cmake -S ./ -B $bdir -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -DCMAKE_BUILD_TYPE=BIT &&  cmake --build $bdir -j 16
+cmake -S ./ -B $bdir -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -DCMAKE_BUILD_TYPE=BIT -DMPI_OPTS:STRING="-Q;--account=project_465000527;-t;5;-p;debug" && cmake --build $bdir 
