8989e030d93e96197970d98e7577d110d4d48e02
Lucian Anton <antonl2219@gmail.com>
2024-12-10 15:46:36 +0000

    more work in alltroallv, using pointers to various array that keeps the displacements data

--- tests/test_linked_list_no_output.F90
+++ tests/test_linked_list_no_output.F90
@@ -12,6 +12,7 @@ program main
   use linked_list_mod!, only : displ_array_list => list
 
   integer send(1), recv(1), req
+  logical copy
 
   type(list_manager), pointer :: list => displ_array_list 
 
@@ -20,13 +21,13 @@ program main
   recv = 1
   req = 1
 
-  call list%append(send,recv,req)
+  call list%append(req,send,recv,copy=.true.)
   
   send = 2
   recv = 2
   req = 2
 
-  call list%append(send,recv,req)
+  call list%append(req, send, recv, copy=.true.)
   if ( list%list_size /= 2 .or. list%head%req /= 2 &
        .or. list%head%send(1) /= 2 .or. list%head%recv(1) /= 2) FAIL("append 2 nodes failed")
   !call list%print_list()
@@ -36,18 +37,27 @@ program main
   if ( list%list_size /= 1 .or. list%head%req /= 1 &
        .or. list%head%send(1) /=1 .or. list%head%recv(1) /=1) FAIL("head remove failed")
       
-  call list%append(send,recv,req)
+  call list%append(req, send, recv, copy=.true.)
   
   send = 4
   recv = 4
   req = 4
 
-  call list%append(send,recv,req)
+  call list%append(req,send,recv,copy)
   call list%remove_req(2)
   if ( list%list_size /= 2 .or. list%head%req /= 4 ) FAIL("try to remove non-existent request failed")
-  
+
   call list%remove_req(1)
   if ( list%list_size /= 1 .or. list%head%req /= 4 ) FAIL("try to remove inner node failed")
+  
+  send = 5
+  recv = 5
+  req = 5
+
+  call list%append(req,send,copy=copy)
+  call list%append(req,recv,copy=copy)
+  if ( list%list_size /= 2 .or. list%head%send(1) /= 5 &
+    .or. list%head%recv(1) /= 5 ) FAIL("try one update in two steps failed")
   
   call list%clear_list()
   if ( associated(list%head) .or. list%list_size /=0 ) FAIL("clear list failed")
