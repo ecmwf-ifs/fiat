5dc005d3f620d25c9488418c902f55f7f879c253
Andrew Beggs <andrew.beggs@ecmwf.int>
2026-02-24 16:37:08 +0000

    Add hints to FindROCTX.cmake for modern ROCm structure

--- cmake/FindROCTX.cmake
+++ cmake/FindROCTX.cmake
@@ -6,10 +6,10 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-set( HAVE_ROCPROFILER_SDK_ROCTX 0)	
+set( HAVE_ROCPROFILER_SDK_ROCTX 0)
 set( ROCTX_REQUIRED_VARS ROCTX_LIBRARIES )
 
-find_package( rocprofiler-sdk-roctx CONFIG PATHS ${ROCM_PATH}/lib )
+find_package( rocprofiler-sdk-roctx CONFIG PATHS ${ROCM_PATH}/lib ${ROCM_PATH}/lib/cmake )
 if( NOT rocprofiler-sdk-roctx_FOUND )
     ecbuild_info( "rocprofiler-sdk-roctx libraries not found" )
     if ( NOT DEFINED ROCM_PATH OR NOT ROCM_PATH_FOUND )
@@ -21,7 +21,7 @@ if( NOT rocprofiler-sdk-roctx_FOUND )
         ecbuild_info( "ROCM path: ${ROCM_PATH}" )
     endif()
 
-    find_path( ROCTX_INCLUDE_DIRS NAMES roctx.h HINTS ${ROCM_PATH}/include/roctracer/ )
+    find_path( ROCTX_INCLUDE_DIRS NAMES roctx.h HINTS ${ROCM_PATH}/include/roctracer/ ${ROCM_PATH}/include/rocprofiler-sdk-roctx )
     list( APPEND ROCTX_REQUIRED_VARS ROCTX_INCLUDE_DIRS )
     find_path( ROCTX_LIBRARY_PATH NAMES libroctx64.so HINTS ${ROCM_PATH}/lib/ )
 
@@ -31,8 +31,8 @@ if( NOT rocprofiler-sdk-roctx_FOUND )
 else()
     if( TARGET ${rocprofiler-sdk-roctx_LIBRARIES} )
         set( ROCTX_LIBRARIES ${rocprofiler-sdk-roctx_LIBRARIES} )
-	set( ROCTX_INCLUDE_DIRS ${rocprofiler-sdk-roctx_INCLUDE_DIR} )
-	list( APPEND ROCTX_REQUIRED_VARS ROCTX_INCLUDE_DIRS )
+    set( ROCTX_INCLUDE_DIRS ${rocprofiler-sdk-roctx_INCLUDE_DIR} )
+    list( APPEND ROCTX_REQUIRED_VARS ROCTX_INCLUDE_DIRS )
         set( HAVE_ROCPROFILER_SDK_ROCTX 1 )
     endif()
 endif()
