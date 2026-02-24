1a60bff032237bf449e113e3c13de5bf9570c520
Andrew Beggs <andrew.beggs@ecmwf.int>
2026-02-24 17:43:30 +0000

    Refactor finding ROCM_PATH so find_package( rocprofiler-sdk-roctx ...) can use it

--- cmake/FindROCTX.cmake
+++ cmake/FindROCTX.cmake
@@ -9,17 +9,18 @@
 set( HAVE_ROCPROFILER_SDK_ROCTX 0)
 set( ROCTX_REQUIRED_VARS ROCTX_LIBRARIES )
 
-find_package( rocprofiler-sdk-roctx CONFIG PATHS ${ROCM_PATH}/lib ${ROCM_PATH}/lib/cmake )
-if( NOT rocprofiler-sdk-roctx_FOUND )
-    ecbuild_info( "rocprofiler-sdk-roctx libraries not found" )
-    if ( NOT DEFINED ROCM_PATH OR NOT ROCM_PATH_FOUND )
-        find_path(
+if ( NOT DEFINED ROCM_PATH OR NOT ROCM_PATH_FOUND )
+    find_path(
             ROCM_PATH
             NAMES include/roctracer/roctx.h include/rocprofiler-sdk-roctx/roctx.h
             HINTS ENV ROCM_DIR ENV ROCM_PATH ENV HIP_PATH ENV ROCM_ROOT_DIR /opt/rocm
-        )
-        ecbuild_info( "ROCM path: ${ROCM_PATH}" )
-    endif()
+    )
+    ecbuild_info( "ROCM path: ${ROCM_PATH}" )
+endif()
+
+find_package( rocprofiler-sdk-roctx CONFIG PATHS ${ROCM_PATH}/lib ${ROCM_PATH}/lib/cmake )
+if( NOT rocprofiler-sdk-roctx_FOUND )
+    ecbuild_info( "rocprofiler-sdk-roctx libraries not found" )
 
     find_path( ROCTX_INCLUDE_DIRS NAMES roctx.h HINTS ${ROCM_PATH}/include/roctracer/ ${ROCM_PATH}/include/rocprofiler-sdk-roctx )
     list( APPEND ROCTX_REQUIRED_VARS ROCTX_INCLUDE_DIRS )
